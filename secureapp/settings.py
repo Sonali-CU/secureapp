f70c3559ec410b140a3ca65310326fd4a812875d
Sonali_Gupta <sonallig04102004@gmail.com>
2025-08-22 22:14:00 +0530

    Update settings.py

--- secureapp/settings.py
+++ secureapp/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^f!51t3mkxh%s_djurlyp)80xj#o*963!lhk^$w24d*1zz^mgc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -120,11 +118,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# Email settings for OTP
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "yourgmail@gmail.com"  # replace with your Gmail
-EMAIL_HOST_PASSWORD = "your-16-char-app-password"  # replace with App Password
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
